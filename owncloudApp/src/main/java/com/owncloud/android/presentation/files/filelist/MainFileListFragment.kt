ae96c3abab1019ebd197a0d8bc9c325c6db39520
Himanshu Gupta <himu.gupta23@gmail.com>
2026-06-18 12:37:52 +0300

    Fix action mode top bar edge-to-edge in landscape
    Signed-off-by: Himanshu Gupta <himu.gupta23@gmail.com>

--- owncloudApp/src/main/java/com/owncloud/android/presentation/files/filelist/MainFileListFragment.kt
+++ owncloudApp/src/main/java/com/owncloud/android/presentation/files/filelist/MainFileListFragment.kt
@@ -46,12 +46,17 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.R as AppCompatR
 import androidx.appcompat.view.ActionMode
 import androidx.appcompat.widget.SearchView
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
 import androidx.core.widget.doOnTextChanged
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
@@ -200,6 +205,7 @@ class MainFileListFragment : Fragment(),
                 isFocusableInTouchMode = false
                 visibility = View.INVISIBLE
             }
+            binding.root.post { adaptActionModeForInfiniteEdges() }
             val inflater = requireActivity().menuInflater
             inflater.inflate(R.menu.file_actions_menu, menu)
             this@MainFileListFragment.menu = menu
@@ -315,6 +321,31 @@ class MainFileListFragment : Fragment(),
 
             fileListAdapter.clearSelection()
         }
+    }
+
+    private fun adaptActionModeForInfiniteEdges() {
+        val actionModeBar = requireActivity().findViewById<View>(
+            AppCompatR.id.action_mode_bar
+        ) ?: return
+        ViewCompat.setOnApplyWindowInsetsListener(actionModeBar) { view, insets ->
+            val systemInsets = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+            )
+
+            view.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = -systemInsets.left
+                topMargin = -systemInsets.top
+                rightMargin = -systemInsets.right
+            }
+            view.updatePadding(
+                left = systemInsets.left,
+                top = systemInsets.top,
+                right = systemInsets.right,
+            )
+
+            insets
+        }
+        ViewCompat.requestApplyInsets(actionModeBar)
     }
 
     override fun onCreateView(
@@ -1675,4 +1706,3 @@ class MainFileListFragment : Fragment(),
         }
     }
 }
-
